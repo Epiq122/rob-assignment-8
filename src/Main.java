76b1e83f41e44f268d12b21ca746359ae9d42554
Robert Gleason <l2ob122@gmail.com>
2022-10-05 19:51:30 -0700

    output seems to be showing up

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Assignment8 assignment8 = new Assignment8();
+
+        List<Integer> numbersList = new ArrayList<>();
+        numbersList = Collections.synchronizedList(numbersList);
+
+        List<CompletableFuture<Void>> tasks = new ArrayList<>();
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 1000; i++ ){
+            CompletableFuture<Void> task =
+                    CompletableFuture.supplyAsync(assignment8::getNumbers,pool).thenAccept(numbersList::addAll);
+            tasks.add(task);
+        }
+
+        Map<Integer, Integer> numMap = numbersList.stream().collect(Collectors.toMap(Function.identity(),sameValue -> 1,
+                Integer::sum));
+        System.out.println(numMap);
+
     }
-}
+
+    }
